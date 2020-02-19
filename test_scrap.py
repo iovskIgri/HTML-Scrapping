c2ef372386e682e2c709b0972e36567751f939b1
sergej.gorbushin <wusawabusa@googlemail.com>
2020-02-19 20:36:34 +0100

    filename wird anhand von feature festgelegt, dokumentation beigefügt

--- test_scrap.py
+++ test_scrap.py
@@ -1,14 +1,17 @@
 import requests
 from lxml import html
-
+#Anfrage an die Website
 page = requests.get("https://www.zeit.de/politik/ausland/2020-02/julian-assange-usa-donald-trump-begnadigung-russland")
+#Datentruktur speichert den Seiteninhalt
 tree = html.fromstring(page.content)
-
+#Name des gesuchten Features
 node_feat = '//p[@class="paragraph article__item"]/text()'
+#Isolierung des Featurenamens
+name_split = node_feat.split('"')
+filename = name_split[1]
+#Seiteninhalt
 text = tree.xpath(node_feat)
-
-print(text)
-
-with open("paragraph article__item.txt","w") as file:
+#Erstellung einer Datei und Speicherung des Texts
+with open(filename + ".txt","w") as file:
     for char in text:
         file.write(char)
