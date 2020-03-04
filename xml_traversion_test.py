68ad50949c11dfe996d2650997d61d8b1bd154ec
Sergej Gorbushin <wusawabusa@googlemail.com>
2020-03-04 20:13:59 +0100

    Datei aus gesammelten Artikeln kann mit richtigen Daten geschrieben werden. Code aufgeräumt. Kommentare hinzugefügt.

--- xml_traversion_test.py
+++ xml_traversion_test.py
@@ -1,29 +1,19 @@
 from lxml import html, etree
-
+#Objekt durchsucht den HTML Baum.
 parser = etree.HTMLParser()
-
+#Baumobjekt das mit dem zuvor definierten Parser durchsucht werden kann.
 tree = etree.parse("lxml_test_scrap.xml", parser)
 
-root = tree.getroot()
-
+#Definitionen der gesuchten HTML Tags als Listen. Xpath-Ausdruck gibt den Text aller Baumknoten wieder, die das definierte Tag haben.
 elements_text = tree.xpath('//p[@class="zon-teaser-standard__text"]/text()')
 elements_title = tree.xpath('//span[@class="zon-teaser-standard__title"]/text()')
-elements_kicker = tree.xpath('//span[@class="zon-teaser-standard__kicker"]/text()')
-elements_article = list()
 
-for i in range(0, len(elements_title)-1):
-    for i in range(0,len(elements_text)-1):
-        elements_article.append(elements_title[i])
-        elements_article.append(elements_text[i])
 
-'{}{}{}{}'.format()
-
-#print(elements_kicker[1])
-#print(elements_title[1])
-#print(elements_text[1])
+#Schreibe die Datei mit gegebenen Daten in gegebenes Format.
+with open("articles.txt","w+") as file:
+    for i in range(0, len(elements_title)-1):
+        file.write("{}{}{}{}".format(elements_title[i], "\n", elements_text[i], "\n\n"))
 
 #print(elements_article)
 print(len(elements_title))
 print(len(elements_text))
-print(len(elements_article))
-print(elements_article)
